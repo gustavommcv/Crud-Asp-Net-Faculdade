5ec0802cd9df2ef893edceb4bcf944bc143c61f3
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2024-09-09 18:14:51 -0300

    Implementing Edit for clients

--- Imobiliaria/Controllers/ClientsController.cs
+++ Imobiliaria/Controllers/ClientsController.cs
@@ -14,6 +14,7 @@ namespace Imobiliaria.Controllers
             _context = context;
         }
 
+        // Create
         [HttpGet]
         [Route("[action]")]
         public IActionResult Create()
@@ -21,18 +22,83 @@ namespace Imobiliaria.Controllers
             return View();
         }
 
+        // Read
         [Route("[action]/{id:int}")]
         public IActionResult Details(int id)
         {
 
             var client = _context.Clients.FirstOrDefault(c => c.Id == id);
-
-            foreach (var item in client.Properties)
+            if (client == null)
             {
-                Console.WriteLine(item);
+                return NotFound();
             }
 
             return View(client);
         }
+
+        // Update
+        [HttpGet]
+        [Route("[action]/{id:int}")]
+        public IActionResult Edit(int id)
+        {
+            var client = _context.Clients.FirstOrDefault(c => c.Id == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return View(client);
+        }
+
+        // (using POST for simplicity in this case)
+        [HttpPost]
+        [Route("[action]/{id:int}")]
+        public IActionResult Edit(Client model)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = _context.Clients.FirstOrDefault(c => c.Id == model.Id);
+                if (client != null)
+                {
+                    client.FirstName = model.FirstName;
+                    client.LastName = model.LastName;
+                    client.Email = model.Email;
+
+                    return RedirectToAction("Details", new { id = client.Id });
+                }
+                return NotFound();
+            }
+            return View(model);
+        }
+
+        // Delete
+        [HttpGet]
+        [Route("[action]/{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var client = _context.Clients.FirstOrDefault(c => c.Id == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return View(client);
+        }
+
+
+        // Delete (POST)
+        [HttpPost]
+        [Route("[action]/{id:int}")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var client = _context.Clients.FirstOrDefault(c => c.Id == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clients.Remove(client);
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
